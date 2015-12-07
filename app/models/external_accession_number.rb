bd31023c6a35e8d10f83190aeb7597bfb7e45bca
phundi <kennethkapundi1@gmail.com>
2015-12-07 08:00:19 +0200

    new models

---
+++ app/models/external_accession_number.rb
@@ -0,0 +1,4 @@
+class ExternalAccessionNumber < ActiveRecord::Base
+  self.table_name = "external_accession_numbers"
+
+end
---
+++ app/models/external_patient_identifier.rb
@@ -0,0 +1,4 @@
+class ExternalPatientIdentifier < ActiveRecord::Base
+  self.table_name = "external_patient_identifiers"
+
+end
