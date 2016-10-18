b54c9617043a9c160248e163bb11177875f8b0b9
phundi <kennethkapundi1@gmail.com>
2016-10-18 08:10:25 +0200

    Custom connection for script in bin; encryption init

--- bin/encryption_init.rb
+++ bin/encryption_init.rb
@@ -1,4 +1,9 @@
 
+ActiveRecord::Base.establish_connection(
+    Rails.configuration.database_configuration["#{Rails.env}"].inject({}){|h, v|
+      h[v.first] = (v.last.to_s.length > 40 ? EncryptionWrapper.humanize(v.last) : v.last); h
+    })
+
 def start
 
   patients =
