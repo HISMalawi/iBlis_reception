19859a709412f6a1a7afc5d8e923485368a40826
phundi <kennethkapundi1@gmail.com>
2017-03-10 14:53:01 +0200

    Resetting soundex codes

---
+++ bin/reset_soundex_codes.rb
@@ -0,0 +1,21 @@
+def start
+
+  patients = Patient.all
+  patients.each_with_index do |p, i|
+    name = p.name.strip.split(/\s+/) rescue next
+    puts "#{(i + 1)}"
+
+    first_name = name.first
+    last_name = name.length > 1 ? name.last : ""
+
+    p.first_name_code = first_name.soundex rescue nil
+    p.last_name_code = last_name.soundex rescue nil
+    p.save
+  end
+end
+
+t = Time.now.to_s(:db)
+puts "Starting batch encryption at #{Time.now.to_s(:db)}"
+start
+puts "Done"
+puts "Started #{t}, Finished #{Time.now.to_s(:db)}"
